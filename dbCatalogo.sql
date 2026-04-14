f97f04daa23ca7834184a0fd5e14f21a01537a22
jmarot-udesa <jmarot@udesa.edu.ar>
2026-04-13 23:17:19 -0300

    sql cometnarios y detalle compra

--- dbCatalogo.sql
+++ dbCatalogo.sql
@@ -21,4 +21,25 @@ FOREIGN KEY (idUsuario) REFERENCES usuarios(id)
 );
 
 
+CREATE TABLE detalle_compra (
+	id INT unsigned PRIMARY KEY auto_increment,
+    idUsuario INT unsigned NOT NULL,
+FOREIGN KEY (idUsuario) REFERENCES usuarios(id),
+	idProducto INT unsigned NOT NULL,
+FOREIGN KEY (idProducto) REFERENCES products(id)
+);
+
+CREATE TABLE comentarios (
+  id INT unsigned PRIMARY KEY auto_increment,
+  idProducto INT unsigned NOT NULL,
+  idUsuario INT unsigned NOT NULL,
+  texto TEXT NOT NULL,
+  createdAt timestamp DEFAULT current_timestamp,
+  updatedAt timestamp DEFAULT "0000-00-00 00:00:00" ON UPDATE current_timestamp,
+  deletedAt timestamp DEFAULT "0000-00-00 00:00:00" ON UPDATE current_timestamp,
+  FOREIGN KEY (idProducto) REFERENCES products(id),
+  FOREIGN KEY (idUsuario) REFERENCES usuarios(id)
+);
+
+
 
